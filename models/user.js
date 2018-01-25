b5da1a6a005fc159999908ee4c4db388fb1be4da
clurdablur <coucc15@gmail.com>
2018-01-25 15:08:48 -0600

    created user profile

--- models/user.js
+++ models/user.js
@@ -3,7 +3,12 @@ var passportLocalMongoose = require("passport-local-mongoose");
 
 var UserSchema = new mongoose.Schema({
     username: String,
-    password: String
+    password: String,
+    avatar: {type: String, default: "https://thesocietypages.org/socimages/files/2009/05/nopic_192.gif"},
+    firstName: String,
+    lastName: String,
+    email: String,
+    isAdmin: {type: Boolean, default: false}
 });
 
 UserSchema.plugin(passportLocalMongoose);
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require("express");
 var router = express.Router();
 var passport = require("passport");
 var User = require("../models/user");
+var Campground = require("../models/campground");
 
 // Root Route
 router.get("/", function(req, res){
@@ -18,9 +19,20 @@ router.get("/login", function(req, res){
    res.render("login", {page: 'login'}); 
 });
 
-// Sign up Logic Route
+// Sign up Logic 
 router.post("/register", function(req, res){
-    var newUser = new User({username: req.body.username});
+    var newUser = new User({
+        username: req.body.username, 
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        avatar: req.body.avatar
+    });
+
+    if(req.body.adminCode === 'secretcode123'){
+        newUser.isAmdin = true;
+    }
+
     User.register(newUser, req.body.password, function(err, user){
         if(err){
             console.log(err);
@@ -46,6 +58,23 @@ router.get("/logout", function(req, res){
     req.logout();
     req.flash("success", "Logged out!");
     res.redirect("/campgrounds");
+});
+
+// User Profile
+router.get("/users/:id", function(req, res){
+    User.findById(req.params.id, function(err, foundUser){
+        if(err){
+            req.flash("error", "Something went wrong.");
+            res.redirect("back");
+        }
+        Campground.find().where('author.id').equals(foundUser._id).exec(function(err, campgrounds){
+            if(err){
+                req.flash("error", "Something went wrong.");
+                res.redirect("back");
+            }
+            res.render("users/show", {user: foundUser, campgrounds: campgrounds});
+        });
+    });
 });
 
 module.exports = router;
